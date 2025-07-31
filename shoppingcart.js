33cb29cfecdd2276ae7823ac0661235b2772ca15
Zena Wu <organictissuemaker@users.noreply.github.com>
2025-07-31 12:22:47 -0700

    index.html file added for deployment

---
+++ shoppingcart.js
@@ -0,0 +1,42 @@
+// get HTML elements
+const storeItemsElement = document.getElementById("store-items")
+const cartTotalElement = document.getElementById("cart-total");
+const cartListElement = document.getElementById("cart");
+// initialize variable for cart total
+let cartTotal = 0;
+// hold store items and their prices in an object
+const inventory = {
+   bread: 1.00,
+   oreos: 2.00,
+   coffee: 3.00
+}
+
+// helper function for creating a new element with a text node
+const createTextElement = (elementType, text) => {
+   const newElement = document.createElement(elementType);
+   const newTextNode = document.createTextNode(text);
+   newElement.appendChild(newTextNode);
+   return newElement;
+}
+
+const updateCartTotal = (item) => {
+   cartTotal += inventory[item];
+   cartTotalElement.innerHTML = cartTotal;
+}
+
+// add an item to the shopping cart list
+const updateCart = (event) => {
+   const buttonText = event.target.textContent;
+   const newCartItem = createTextElement("li", buttonText);
+   cartListElement.appendChild(newCartItem);
+   updateCartTotal(buttonText)
+}
+
+// create store item HTML elements with event listeners
+for (item in inventory) {
+   const storeItem = createTextElement("button", item)
+   storeItem.type = "button";
+   storeItemsElement.appendChild(storeItem);
+   storeItem.addEventListener("click", (event) => updateCart(event))
+}
+
